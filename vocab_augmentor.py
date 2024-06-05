c5cc06bdb98855b55919a570927645ba99b59f1c
Raul <9953156+raul23@users.noreply.github.com>
2024-06-05 18:35:26 -0400

    Update vocab_augmentor.py

--- vocab_augmentor.py
+++ vocab_augmentor.py
@@ -1,2 +1,12 @@
+import logging
+# Suppress the specific warning by setting the logging level to ERROR
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
+import torch
+import transformers
+
+from huggingface_hub import notebook_login
+
 if __name__ == "__main__":
-    pass
+    # Login to Hugging Face
+    notebook_login()
